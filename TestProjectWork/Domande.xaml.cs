ef6e26e9b0039fce7df54eba3f5389b2b3b35d56
Ascanio Donati <donatiascanio@gmail.com>
2019-01-30 20:41:52 +0100

    Aggiungere i file di progetto.

---
+++ TestProjectWork/Domande.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace TestProjectWork
+{
+    /// <summary>
+    /// Logica di interazione per Domande.xaml
+    /// </summary>
+    public partial class Domande : Window
+    {
+        string json;
+        List<Query> domande;
+        bool daSalvare = false;
+
+        public Domande()
+        {
+            InitializeComponent();
+            Imposta();
+            ImportaDomande();
+        }
+
+        void Imposta()
+        {
+            
+        }
+
+        void ImportaDomande()
+        {
+            StreamReader sr = new StreamReader("Files\\Domande.json");
+            json = sr.ReadToEnd();
+            sr.Close();
+
+            domande = JsonConvert.DeserializeObject<List<Query>>(json);
+
+            dgrDomande.ItemsSource = domande;
+
+            /*dgrDomande.Columns[0].Header = "Domanda";
+            dgrDomande.Columns[1].Header = "Modificabile?";*/
+        }
+
+
+        private void btnEditFields_Click(object sender, RoutedEventArgs e)
+        {
+            btnEditFields.IsEnabled = false;
+            dgrDomande.IsReadOnly = false;
+        }
+
+        private void btnCloseWithoutSaving_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCloseWIthSaving_Click(object sender, RoutedEventArgs e)
+        {
+            List<Query> queries = new List<Query>();
+
+            foreach(Query query in dgrDomande.ItemsSource)
+            {
+                queries.Add(query);
+            }
+
+            StreamWriter sw = new StreamWriter("Files\\Domande.json");
+            sw.AutoFlush = true;
+            sw.Write(JsonConvert.SerializeObject(queries));
+            sw.Close();
+        }
+
+        private void grdPrincipal_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("notepad", Environment.CurrentDirectory + "\\Files\\Domande.json");
+        }
+    }
+}
---
+++ TestProjectWork/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace TestProjectWork
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string url = "https://api.api.ai/v1/query?v=20150910";
+        string token = "cb7af0ed21dd41d9bac5c64afcc45b27";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            //lblResponse.Content = GetResult(txtQuestion.Text);
+
+        }
+
+        public string GetResult(string query)
+        {
+            string json = DoRequest(query, "en", "somerandomthing");
+            QueryResult qr = JsonConvert.DeserializeObject<QueryResult>(json);
+            return qr.result.fulfillment.speech;
+        }
+
+        public string DoRequest(string text, string language, string sessionID)
+        {
+
+            Dictionary<string, string> body = new Dictionary<string, string>
+            {
+                { "query", text },
+                { "lang", language },
+                { "sessionId", sessionID }
+            };
+
+            var data = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+            var result = "";
+
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            var response = client.PostAsync(url, data).Result;
+
+            using (HttpContent content = response.Content)
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+                return result.ToString();
+            }
+
+        }
+
+        private void btnDomande_Click(object sender, RoutedEventArgs e)
+        {
+            Domande dom = new Domande();
+            dom.ShowDialog();
+        }
+    }
+}
