c2f3c289777ab3b948f9a9cf4c823c5c1fc9a06e
Ascanio Donati <donatiascanio@gmail.com>
2019-02-05 11:29:27 +0100

    Revert "Aggiornato BotWindow"
    This reverts commit 2a0095d3c3c53a5bd195e993de0d9124bfa12e15.

--- TestProjectWork/BotWindow.xaml.cs
+++ TestProjectWork/BotWindow.xaml.cs
@@ -42,44 +42,44 @@ namespace TestProjectWork
             g.ColumnDefinitions.Add(new ColumnDefinition());
             
             g.VerticalAlignment = VerticalAlignment.Top;
-
-            Grid g1 = new Grid();
-            g1.SetValue(Grid.ColumnSpanProperty, 2);
-            g1.SetValue(Grid.ColumnProperty, 2);
-
-            g1.Children.Add(tb);
-            g.Children.Add(g1);
-            w.Children.Add(g);
-
-        }
-
-        public void AddReply(string text, double maxWidth)
-        {
-
-            TextBlock tb = new TextBlock();
-            tb.HorizontalAlignment = HorizontalAlignment.Right;
-            tb.FontSize = 16;
-            tb.Background = Brushes.LightBlue;
-            tb.Margin = new Thickness(5, 5, 5, 5);
-            tb.TextWrapping = TextWrapping.Wrap;
-            tb.Text = text;
-
-            Grid g = new Grid();
-            g.ColumnDefinitions.Add(new ColumnDefinition());
-            g.ColumnDefinitions.Add(new ColumnDefinition());
-            g.ColumnDefinitions.Add(new ColumnDefinition());
-
-            g.VerticalAlignment = VerticalAlignment.Top;
+            g.Width = uc.Width;
+            g.Children.Add(tb);
 
             Grid g1 = new Grid();
             g1.SetValue(Grid.ColumnSpanProperty, 2);
             g1.SetValue(Grid.ColumnProperty, 1);
 
+            w.Children.Add(g);
+            g.Children.Add(g1);
+            g1.Children.Add(tb);
+            
+        }
+
+        public void AddReply(string text)
+        {
+
+            TextBlock tb = new TextBlock();
+            tb.HorizontalAlignment = HorizontalAlignment.Left;
+            tb.FontSize = 16;
+            tb.MaxWidth = uc.Width / (4 / 3);
+            tb.Background = Brushes.Yellow;
+            tb.Margin = new Thickness(5, 5, 5, 5);
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.Text = text;
+
+            Grid g1 = new Grid();
+            g1.SetValue(Grid.ColumnSpanProperty, 2);
+
+            Grid g = new Grid();
+            g.ColumnDefinitions.Add(new ColumnDefinition());
+            g.ColumnDefinitions.Add(new ColumnDefinition());
+            g.ColumnDefinitions.Add(new ColumnDefinition());
+            g.VerticalAlignment = VerticalAlignment.Top;
+            g.Width = uc.Width;
+
             g1.Children.Add(tb);
             g.Children.Add(g1);
             w.Children.Add(g);
-
         }
-
     }
 }
